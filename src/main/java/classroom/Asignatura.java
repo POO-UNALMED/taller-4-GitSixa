cc7b87590324feb2975ba4eddab7d066046f611f
Sixa <mrjoer22@gmail.com>
2020-09-08 21:20:55 -0500

    Primer intento

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -8,11 +8,11 @@ public class Asignatura {
     int codigoExterno;
 
     Asignatura() {
-        this("Sin nombre");
-        this(0);
+    	this("Sin nombre");
+        
     }
 
-    Asignatura(int codigoInterno) {
+    Asignatura(byte codigoInterno) {
         this("Sin nombre", codigoInterno, 0);
     }
 
@@ -36,10 +36,10 @@ public class Asignatura {
         this.nombre = nombre;
     }
 
-    void cambiarDatos(int codigoInterno) {
+    void cambiarDatos(byte codigoInterno) {
         this.codigoInterno = codigoInterno;
     }
-
+    
     void cambiarDatos(int codigoExterno) {
         this.codigoExterno = codigoExterno;
     }
